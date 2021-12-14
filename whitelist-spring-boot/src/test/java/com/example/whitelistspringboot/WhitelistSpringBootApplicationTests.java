87e2c271394d8cf93d523051e987e79631e9bb25
yachao.ni <yachao.ni@ipinyou.com>
2021-12-14 13:52:09 +0800

    配置文件

---
+++ whitelist-spring-boot/src/test/java/com/example/whitelistspringboot/WhitelistSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whitelistspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhitelistSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
